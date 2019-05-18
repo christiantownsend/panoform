31581e2de73a38d915507863d47ef8ac991f577a
BuildTools <unconfigured@null.spigotmc.org>
2019-05-18 02:01:38 -0400

    Updates

---
+++ precache-manifest.4102cf1c6205e9504f91369f42f0f18d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e0ac63307bb03a72dffe",
+    "url": "/js/chunk-vendors.af3c2bdf.js"
+  },
+  {
+    "revision": "d6b80cf1fd264e4a320f",
+    "url": "/js/app.35900f5a.js"
+  },
+  {
+    "revision": "87ee59f069d001b9c08be69f830e6c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69cfce8f20fe9076d8a89bf228ad5b21",
+    "url": "/img/pic.69cfce8f.png"
+  },
+  {
+    "revision": "d6b80cf1fd264e4a320f",
+    "url": "/css/app.82b58fb5.css"
+  }
+];
