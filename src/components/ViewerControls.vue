bc6d6c4ca8443b515010206b9f2ccb5ee05d394d
BuildTools <unconfigured@null.spigotmc.org>
2019-05-21 10:30:57 -0400

    Added control icons

--- src/components/ViewerControls.vue
+++ src/components/ViewerControls.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="panoform-controls">
-    <div class="panoform-controlbar top">
-      <button v-on:click.prevent="closeViewer">Close</button>
+  <div class="controls">
+    <div class="controlbar top">
+      <button
+        class="button close enabled"
+        v-on:click.prevent="closeViewer"
+      ></button>
     </div>
-    <div class="panoform-controlbar">
-      <button v-if="webVR" v-on:click.prevent="toggleWebVR">
-        Toggle WebVR
-      </button>
-      <button v-if="deviceOrientationVR" v-on:click.prevent="toggleVR">
-        Toggle Stereo
-      </button>
+    <div class="controlbar">
+      <button
+        class="button VR"
+        :class="{ enabled: isWebVR }"
+        v-if="webVR"
+        v-on:click.prevent="toggleWebVR"
+      ></button>
+      <button
+        class="button VR"
+        :class="{ enabled: isStereo }"
+        v-if="deviceOrientationVR"
+        v-on:click.prevent="toggleVR"
+      ></button>
     </div>
   </div>
 </template>
@@ -23,6 +32,14 @@ export default {
       deviceOrientationVR: false,
       image: require("@/assets/pic.png")
     };
+  },
+  computed: {
+    isStereo() {
+      return this.$store.state.viewer.stereo;
+    },
+    isWebVR() {
+      return this.$store.state.viewer.webVR;
+    },
   },
   methods: {
     init() {
@@ -82,3 +99,65 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.controls {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+
+.controlbar {
+  position: absolute;
+  top: auto;
+  bottom: 0;
+  width: 100%;
+  padding: 1em;
+  /* background-color: rgba(0,0,0,.5); */
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  pointer-events: initial;
+
+  &.top {
+    top: 0;
+    bottom: auto;
+    justify-content: flex-start;
+  }
+
+  .button {
+    display: inline-block;
+    border: none;
+    margin: 0;
+    text-decoration: none;
+    background: none;
+    cursor: pointer;
+    text-align: center;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    background-repeat: no-repeat;
+    background-position: center;
+
+    height: 3em;
+    width: 3em;
+
+    filter: brightness(80%);
+
+    &.enabled,
+    &:hover {
+      filter: brightness(100%);
+    }
+
+    &.close {
+      background-image: url("../assets/close-icon.svg");
+    }
+
+    &.VR {
+      background-image: url("../assets/vr-icon.svg");
+    }
+  }
+}
+</style>
